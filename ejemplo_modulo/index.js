c6594d97fd4bd790a70b4bd7d56671100a355cc9
Erika Tavera <dugarerika@gmail.com>
2020-09-09 23:19:58 +0300

    agrego informacion al archivo index.js

--- ejemplo_modulo/index.js
+++ ejemplo_modulo/index.js
@@ -1,5 +1,8 @@
 'use strict'
 
+console.log('Se inicializa la calculadora');
+
 const calculadora = require('./calculadora');
+const calculadora1 = require('./calculadora');
 
 console.log(calculadora.suma(3,4));
