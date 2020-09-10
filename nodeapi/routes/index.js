63db8ba4b92897ffc0b88eb99a67c2ce52b3900a
Erika Tavera <dugarerika@gmail.com>
2020-09-10 18:15:46 +0300

    agrego rutas al archivo index.js

--- nodeapi/routes/index.js
+++ nodeapi/routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/paramenruta/:etiqueta', (req,res,next) => {
+  console.log(req.params);
+  res.send('ok')
+});
+
+router.get('/paramenruta/:numero?', (req,res,next) => {
+  console.log(req.params);
+  res.send('ok opcional')
+});
+
+router.get('/parametros/:color/piso/:piso/puerta/:puerta', (req,res,next) => {
+  console.log(req.params);
+  res.send('ok')
+});
+
+
 module.exports = router;
