992a8784a754cc5dde5be24b3ff10f74af3786a9
Erika Tavera <dugarerika@gmail.com>
2020-11-06 17:06:09 +0300

    agrego ejemplo donde uso async y await

--- ejemplos/asyncawait.js
+++ ejemplos/asyncawait.js
@@ -13,8 +13,28 @@ function sleep(ms){
 async function main(){
     const resultado = await sleep(3000);
     console.log(resultado);
+
+    const resultado2 = await sleep(1000);
+    console.log(resultado2);
 }
+
+main();
 
 main().catch(err =>{
     console.log('Hubo un error', err);
 });
+
+// await consume una promesa
+
+// async function blucleAsincronoEnSerie(){
+//     for(let i = 0; i<5; i++){
+//         const info = await row.findNext();
+//         console.log(info.name);
+//     }
+// }
+
+// blucleAsincronoEnSerie();
+
+// blucleAsincronoEnSerie().catch(err =>{
+//     console.log('Hubo un error', err);
+// });
